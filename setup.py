75766b78de991938d1feb5701c4ca056b697592e
Nagarjun <nagarjunpr44@gmail.com>
2025-05-06 20:17:04 +0530

    folder structure updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Generative_AI Project',
+    version='0.1',
+    author='Nagarjun',
+    packages=find_packages(),
+    install_requries=[]
+)
